b7d231d4d02a949907c3598cbcb80446210c6874
deyllon ramos <deyllonp986@gmail.com>
2022-04-18 13:43:19 -0300

    Completando a primeira regra do negocio

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import holidays
 
 
 feriados = holidays.Brazil()
-
+folga_impar = {}
+folga_par = {}
 def verifica_ano():
     a=[]
     while len(a) == 0:
@@ -30,28 +31,93 @@ def verifica_ano():
         else:
             print("Porfavor verifique se você digitou um ano ")
         
+def adiciona_folga_impar(dia):
+    impar = ["rafael" , "neto", "henrique"]
+    
+    dia_somar = timedelta(days=1)
+    while len(impar) !=0:
+        nome_do_dia = day_name[dia.weekday()]
+        if dia.day % 2 != 0:
+            while True:
+                alguem_quer_folga = input(f"Digite 1 se alguem quiser folgar no dia {dia} ou então 2 se ninguem quiser: ")
+                if alguem_quer_folga == "1":
+                    while True:
+                        quem_quer_folga = input("Digite o nome de quem quer folga : ")
+                        if quem_quer_folga in impar:
+                            if f"{nome_do_dia}-{dia.day}" not in folga_impar:
+                                folga_impar.update({f"{nome_do_dia}-{dia.day}" : f"{quem_quer_folga}"})
+                                impar.remove(quem_quer_folga)
+                                break
+                            else:
+                                print(f"já tem alguem de folga no dia {nome_do_dia}-{dia.day}")
+                                break
+                        print(f"Porfavor verifique se você digitou o nome certo na lista {impar}")
+                    break
+                elif alguem_quer_folga == "2":
+                    break
+                
+                print("Porfavor digite um digito valido")
+        dia+= dia_somar
         
-impar = ["Rafael" , "Neto", "Henrique"]
-par = ["Deyllon", "Bruno", "Vila"]
+    return folga_impar
+        
+def adiciona_folga_par(dia):
+    par = ["Deyllon", "Bruno", "Vila"]
+    
+    dia_somar = timedelta(days=1)
+    while len(par) !=0:
+        nome_do_dia = day_name[dia.weekday()]
+        if dia.day % 2 == 0:
+            while True:
+                alguem_quer_folga = input(f"Digite 1 se alguem quiser folgar no dia {dia} ou então 2 se ninguem quiser: ")
+                if alguem_quer_folga == "1":
+                    while True:
+                        quem_quer_folga = input("Digite o nome de quem quer folga : ")
+                        if quem_quer_folga in par:
+                            if f"{nome_do_dia}-{dia.day}" not in folga_par:
+                                folga_par.update({f"{nome_do_dia}-{dia.day}" : f"{quem_quer_folga}"})
+                                par.remove(quem_quer_folga)
+                                break
+                            else:
+                                print(f"já tem alguem de folga no dia {nome_do_dia}-{dia.day}")
+                                break
+                        print(f"Porfavor verifique se você digitou o nome certo na lista {par}")
+                    break
+                elif alguem_quer_folga == "2":
+                    break
+                
+                print("Porfavor digite um digito valido")
+        dia+= dia_somar
+        
+    return folga_par
+                 
 
 def verifica_se_domingo_e_impar(dia, feriados):
+
     numero_do_dia = dia.day
     if (numero_do_dia % 2 != 0 and day_name[dia.weekday()] == "Sunday") or (numero_do_dia % 2 != 0 and dia in feriados ):
-        print(dia)
-
+       return True
+        
 def verific_se_domingo_e_par(dia, feriado):
     numero_do_dia = dia.day
     if (numero_do_dia % 2 == 0 and day_name[dia.weekday()] == "Sunday") or (numero_do_dia % 2 == 0 and dia in feriados ):
-        print(dia)
+        return True
 
 def ip():
+    
     dias = timedelta(days=1)
     resultado = verifica_ano()
     inicio_do_mes = resultado[1]
     fim_do_mes = resultado[2]
+    a=[]
+    b=[]
     while inicio_do_mes <= fim_do_mes:
-        verifica_se_domingo_e_impar(inicio_do_mes, resultado[0])
-        verific_se_domingo_e_par(inicio_do_mes, resultado[0])
-        inicio_do_mes += dias
+        if verifica_se_domingo_e_impar(inicio_do_mes, resultado[0]) == True:
+            a.append(adiciona_folga_impar(inicio_do_mes))
 
-ip()
+        if verific_se_domingo_e_par(inicio_do_mes, resultado[0]) == True:
+            b.append(adiciona_folga_par(inicio_do_mes))
+            
+        inicio_do_mes += dias
+    return a, b
+print(ip())
